9fab5ac44a960345d9da3e0e1a63309001aff24b
nylageneralsuppliesltd-lab <nylageneralsuppliesltd@gmail.com>
2026-01-07 14:15:45 +0300

    Create helpers.js

---
+++ js/modules/utils/helpers.js
@@ -0,0 +1,12 @@
+// js/utils/helpers.js
+export function formatCurrency(amount) {
+    return 'KSh ' + Number(amount).toLocaleString('en-KE', { minimumFractionDigits: 0 });
+}
+
+export function formatDate(dateString) {
+    return new Date(dateString).toLocaleDateString('en-GB');
+}
+
+export function showAlert(message, type = 'success') {
+    alert(message); // Can upgrade to nice toast later
+}
